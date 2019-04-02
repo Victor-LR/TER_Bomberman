86cd295f3378d60da34283f54ccf33fbdff9874f
Victor-LR <49147934+Victor-LR@users.noreply.github.com>
2019-04-02 16:23:00 +0200

    Add files via upload

---
+++ Bomberman/src/Test_main.java
@@ -0,0 +1,16 @@
+import graphics.Cadre_Jeu;
+
+
+public class Test_main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		Cadre_Jeu fenetre = new Cadre_Jeu();
+		fenetre.setVisible(true);
+
+	}
+
+}
---
+++ Bomberman/src/map/GameState.java
@@ -0,0 +1,69 @@
+package map;
+import java.util.ArrayList;
+
+
+import agents.Agent;
+import agents.AgentAction;
+import agents.AgentType;
+
+public class GameState {
+	
+	Map map;
+	
+	private ArrayList<Agent> ennemies = null;
+	
+	public GameState(Map map){
+		
+		ennemies = new ArrayList<Agent>();
+		
+		this.map=map;
+		
+		for(int i=0;i<map.getNumber_of_ennemies();i++)
+		{
+			Agent a = new Agent(AgentType.ENNEMY, map.getEnnemy_start_x(i), map.getEnnemy_start_y(i) );
+			ennemies.add(a);
+		}
+
+	}
+	
+	public boolean isLegalMove(AgentAction action, Agent agent){
+		int x = action.getVx();
+		int y = action.getVy();
+		if(map.isWall(agent.getX()+x, agent.getY()+y))
+			return false;
+		return true;
+	}
+	
+	public void moveAgent(Agent agent, AgentAction action)
+	{
+		int x = action.getVx();
+		int y = action.getVy();
+		if (isLegalMove(action, agent) ){
+			agent.setX(x+action.getVx());
+			agent.setY(y+action.getVy());
+			if (action.getAction()!=Map.STOP) 
+				agent.setDirection(action.getAction());
+	
+		}
+	}
+	
+	public Agent getAgent(GameState etat, int agentId){
+		
+		for (Agent p : etat.getEnnemies()){
+			if(p.getId() == agentId){
+				return p;
+			}
+		}
+
+		return null;
+	}
+	
+	public ArrayList<Agent> getEnnemies(){
+		return ennemies;
+	}
+	
+	public Map getMap(){
+		return map;
+	}
+
+}

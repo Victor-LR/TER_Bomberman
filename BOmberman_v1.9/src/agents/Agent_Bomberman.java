c8c3c7edf71f3725f7b01bfc1ea43e5058151f06
Kevin-Bal <49147949+Kevin-Bal@users.noreply.github.com>
2019-04-09 17:02:41 +0200

    Add files via upload

---
+++ BOmberman_v1.9/src/agents/Agent_Bomberman.java
@@ -0,0 +1,69 @@
+package agents;
+
+import java.util.ArrayList;
+
+import objets.Objet_Bomb;
+
+import map.GameState;
+import agents.Agent;
+
+public class Agent_Bomberman extends Agent {
+	
+	private int range;
+	private int points;
+	private ArrayList<Objet_Bomb> bombes = null;
+	private int nbBombes;
+
+	public Agent_Bomberman (int px, int py) {
+		super(AgentType.BOMBERMAN, px, py);
+		this.range = 5;
+		this.points = 0;
+		this.bombes = new ArrayList<Objet_Bomb>();
+		this.nbBombes = 5;
+	}
+	
+	public AgentAction chooseAction(GameState etatjeu) 
+	{
+		ArrayList<AgentAction> listAction=new ArrayList<AgentAction>();
+		for(int i=0;i<=5;i++)
+		{
+			if (etatjeu.isLegalMove(new AgentAction(i), this))
+				listAction.add(new AgentAction(i));
+		}		
+		return(listAction.get((int)(Math.random()*listAction.size())));
+	}	
+	
+	public int getRange() {
+		return range;
+	}
+
+
+	public void setRange(int range) {
+		this.range = range;
+	}
+	
+	public int getPoints() {
+		return points;
+	}
+
+	public void setPoints(int point) {
+		this.points = point;
+	}
+	
+	public ArrayList<Objet_Bomb> getBombes() {
+		return bombes;
+	}
+
+	public void setBombes(ArrayList<Objet_Bomb> bombes) {
+		this.bombes = bombes;
+	}
+	
+	public int getNbBombes() {
+		return nbBombes;
+	}
+
+	public void setNbBombes(int nbBombes) {
+		this.nbBombes = nbBombes;
+	}
+	
+}

b5f64c1d98a3a92aeea07e1ea14ed48d777500fc
Kevin-Bal <49147949+Kevin-Bal@users.noreply.github.com>
2019-04-10 17:00:40 +0200

    Add files via upload

---
+++ Bomberman_v2.1/src/Test_main.java
@@ -0,0 +1,26 @@
+import game.BombermanGame;
+import graphics.Cadre_Jeu;
+
+
+public class Test_main {
+
+	/**
+	 * @param args
+	 */
+	
+	public static void main(String[] args) {
+		
+		BombermanGame BbmG = new BombermanGame();
+		try {
+			BbmG.loadFile("./layout/exemple.lay");
+			BbmG.init();
+			BbmG.launch();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		Cadre_Jeu fenetre = new Cadre_Jeu(BbmG);
+		fenetre.setVisible(true);
+
+	}
+
+}
---
+++ Bomberman_v2.1/src/graphics/paint_score.java
@@ -0,0 +1,128 @@
+package graphics;
+
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import game.BombermanGame;
+import game.GameObserver;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+
+
+import agents.Agent_Bomberman;
+
+
+public class paint_score implements GameObserver {
+
+	
+	BombermanGame BbmG;
+	//private GameState Jeu_actuel = null;
+	
+	public JPanel panel = null;
+	public ArrayList<JLabel> listlab;
+	private JButton stop =null;
+	private JButton run =null;
+	private JButton restart =null;
+	private JLabel turn = null;
+	
+	public paint_score(BombermanGame bomberman){
+		
+		this.BbmG = bomberman;
+		
+		panel = new JPanel();
+		panel.setLayout(new FlowLayout());
+		
+		this.BbmG.addObserver((GameObserver)this);
+		//Jeu_actuel = bomberman.etatJeu;
+		
+		
+		listlab = new ArrayList<JLabel>();
+		
+		turn = new JLabel();
+		this.panel.add(this.turn);
+		
+		Icon icon_pause = new ImageIcon("./image/icon_pause.png");
+		stop = new JButton(icon_pause);
+		this.panel.add(this.stop);
+		
+		Icon icon_run = new ImageIcon("./image/icon_run.png");
+		run = new JButton(icon_run);
+		this.panel.add(this.run);
+		
+		Icon icon_restart = new ImageIcon("./image/icon_restart.png");
+		restart = new JButton(icon_restart);
+		this.panel.add(this.restart);
+		
+		ArrayList<Agent_Bomberman> bombermans = this.BbmG.etatJeu.getBombermans();
+		for(int i = 0; i < bombermans.size(); i++){
+			this.listlab.add(new JLabel());
+			panel.add(listlab.get(i));
+		}
+		
+		creer_button();
+	}
+	
+	public void creer_button(){
+		restart.setFocusPainted(false);
+		restart.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent evenement) {
+				try {
+					BbmG.loadFile(BbmG.getMap().getFilename());
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				BbmG.restart();
+			}
+		});
+		
+		run.setFocusPainted(false);
+		run.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent evenement) {
+				System.out.println("	run");
+				BbmG.launch();
+			}
+		});
+		
+		stop.setFocusPainted(false);
+		stop.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent evenement) {
+				BbmG.stop();
+				System.out.println("Stop");
+			}
+		});
+		
+		
+	}
+	 
+	public void affichage_score(Agent_Bomberman ag,int i){
+		listlab.get(i).setText("N°"+(i+1)+" score : "+ag.getPoints()+"    ");
+	}
+
+
+	@Override
+	public void update() {
+		
+		//System.out.println("scores : ");
+		//ArrayList<Agent_Bomberman> bombermans = this.BbmG.etatJeu.getBombermans();
+		
+		for(int i = 0; i < this.BbmG.etatJeu.getBombermans().size(); i++){
+			Agent_Bomberman bomberman = this.BbmG.etatJeu.getBombermans().get(i);
+			affichage_score(bomberman,i);
+			
+			//System.out.println("		test score "+ bomberman.getPoints());
+		}
+		
+		this.turn.setText("Tour : "+this.BbmG.getTurn());
+		
+		
+			
+	}
+}
